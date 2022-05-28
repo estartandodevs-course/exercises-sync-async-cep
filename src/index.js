95d38cf9ce2dd755e5a763176c269a6e3c813e9b
LucasSlv <lucassilvax005x@gmail.com>
2022-05-28 11:23:25 -0300

    utilização do recuso de Async/Await do Javascript.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,19 @@
-/*
-Para este exercício utilize o recurso de Async/Await do Javascript.
-Será necessário buscar as informações em uma api externa (ViaCep).
-Baixar o resultado, tratar o retorno para mostrar logradouro, bairro - localidade. 
-
-Exemplo de resultado obtido na api ViaCep:=
-    {
-      "cep": "01001-000",
-      "logradouro": "Praça da Sé",
-      "complemento": "lado ímpar",
-      "bairro": "Sé",
-      "localidade": "São Paulo",
-      "uf": "SP",
-      "ibge": "3550308",
-      "gia": "1004",
-      "ddd": "11",
-      "siafi": "7107"
-    }
-      
-Exemplo de como a função deve retornar: 
-
-"Praça da Sé, Sé - São Paulo"
-
-Documentação da API:  https://viacep.com.br/
-
-*/
-
 const BASE_API_VIA_CEP = "https://viacep.com.br/ws/";
-
-/*
-    TODO 1:
-    - Implemente a função getAddressByCep com o recurso de Async/Await.
-    - A função deve retornar nesse formato: logradouro, bairro - localidade.
-    - A função deve retornar o erro `CEP não encontrado` caso o cep não seja encontrado.
-*/
+const axios = require('axios')
 
 async function getAddressByCep(cep) {
-  // implemente aqui
+  try {
+    const result = await axios.get(`${BASE_API_VIA_CEP}/${cep}/json/`)
+    const logradouro = (result.data.logradouro);
+    const bairro = (result.data.bairro);
+    const localidade = result.data.localidade;
+    const endereco = `${logradouro}, ${bairro} - ${localidade}`
+    return (endereco)
+  }
+  catch (error) {
+    throw new Error(error.statusText = "CEP não encontrado")
+  }
 }
 
+getAddressByCep('01001000')
 module.exports = { getAddressByCep };
